f3addd563d7b6ce38e7cefdd86f1fa623a4400cc
Grace Hedges <grace.hedges@milliman.com>
2026-07-09 14:19:43 -0400

    update annotation in settings

--- lib/settings.py
+++ lib/settings.py
@@ -1,25 +1,43 @@
-def init() -> None:
-    global version
-    with open('ReadMe.md','r') as f:
-        version = f.read()[10:15]
-    f.close()
-        
-    global ext
-    ext = {'SQL':'.sql','PYTHON':'.py','R':'.r'}
+# classes
+from typing import List
 
-    # initialized in config.ini file
-    global host_url
-    global workspace_path
-    global client_code
-    global client
-    global check_ext
-    global download
-    global export_path
-    global create_file_structure
-    global overwrite
-    global one_file
-    global open_file
-    global scrap_contains
-    global scrap_startswith
-    global scrap_endswith
-    global show_scrap
+global version
+with open('ReadMe.md','r') as f:
+    version = f.read()[10:15]
+f.close()
+    
+global ext
+ext = {'SQL':'.sql','PYTHON':'.py','R':'.r'}
+
+# defined in config.ini file
+global host_url
+global workspace_path
+global client_code
+global client
+global check_ext
+global download
+global export_path
+global create_file_structure
+global overwrite
+global one_file
+global open_file
+global scrap_contains
+global scrap_startswith
+global scrap_endswith
+global show_scrap
+
+host_url: str 
+workspace_path: str
+client_code: str
+client: str
+check_ext: bool
+download: bool
+export_path: str
+create_file_structure: str
+overwrite: bool 
+one_file: bool
+open_file: bool
+scrap_contains: List[str]
+scrap_startswith: List[str]
+scrap_endswith: List[str]
+show_scrap: bool
