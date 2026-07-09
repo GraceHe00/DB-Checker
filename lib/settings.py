f713eb9fa101fffb4336c0e318a9525e1638e8db
Grace Hedges <grace.hedges@milliman.com>
2026-07-09 15:51:17 -0400

    updated main to new format

--- lib/settings.py
+++ lib/settings.py
@@ -1,30 +1,31 @@
 # classes
 from typing import List
 
-global version
-with open('ReadMe.md','r') as f:
-    version = f.read()[10:15]
-f.close()
-    
-global ext
-ext = {'SQL':'.sql','PYTHON':'.py','R':'.r'}
+def init() -> None:
+    global version
+    with open('ReadMe.md','r') as f:
+        version = f.read()[10:15]
+    f.close()
+        
+    global ext
+    ext = {'SQL':'.sql','PYTHON':'.py','R':'.r'}
 
-# defined in config.ini file
-global host_url
-global workspace_path
-global client_code
-global client
-global check_ext
-global download
-global export_path
-global create_file_structure
-global overwrite
-global one_file
-global open_file
-global scrap_contains
-global scrap_startswith
-global scrap_endswith
-global show_scrap
+    # defined in config.ini file
+    global host_url
+    global workspace_path
+    global client_code
+    global client
+    global check_ext
+    global download
+    global export_path
+    global create_file_structure
+    global overwrite
+    global one_file
+    global open_file
+    global scrap_contains
+    global scrap_startswith
+    global scrap_endswith
+    global show_scrap
 
 host_url: str 
 workspace_path: str
