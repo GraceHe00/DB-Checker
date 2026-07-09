2a4e1ec2a29faebff7354343c52a911e374252fc
Grace Hedges <grace.hedges@milliman.com>
2026-07-09 14:03:40 -0400

    remove type annotation

--- lib/settings.py
+++ lib/settings.py
@@ -1,6 +1,3 @@
-# classes
-from typing import List
-
 def init() -> None:
     global version
     with open('ReadMe.md','r') as f:
@@ -26,19 +23,3 @@ def init() -> None:
     global scrap_startswith
     global scrap_endswith
     global show_scrap
-
-    host_url: str
-    workspace_path: str
-    client_code: str
-    client: str
-    check_ext: bool
-    download: bool
-    export_path: str
-    create_file_structure: str
-    overwrite: bool 
-    one_file: bool
-    open_file: bool
-    scrap_contains: List[str]
-    scrap_startswith: List[str]
-    scrap_endswith: List[str]
-    show_scrap: bool
