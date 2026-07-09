89693aa6d441c9e7f65397a2163d4452674fabe5
Grace Hedges <grace.hedges@milliman.com>
2026-07-09 12:19:44 -0400

    databricks_util

---
+++ lib/databricks_util.py
@@ -0,0 +1,17 @@
+import subprocess
+
+def version() -> str | None:
+    """
+    Returns Databricks CLI version; if CLI is not installed, then returns nothing
+    """
+    try: return subprocess.run(['databricks','-v'], capture_output=True, text=True).stdout
+    except: return None
+
+def host_info() -> str | None:
+    """
+    Returns host info
+    """
+    auth_info = subprocess.run(['databricks','auth','describe'],capture_output=True,text=True).stdout
+    host_url= auth_info.split()[1]
+    if host_url == 'to': return None
+    else: return host_url
